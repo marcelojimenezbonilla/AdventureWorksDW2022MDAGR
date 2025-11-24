138f8d316dfd4a0c0ee0b282a5fe1c05a23a6700
Marcelo Jiménez <marcelojimenezb@gmail.com>
2025-11-23 23:54:15 -0400

    actualizacion ETL

---
+++ Staging/01_Create_StagingEmployee.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE Staging.Employee (
+	BusinessEntityID     INT,
+	NationalIDNumber     NVARCHAR(15),
+	LoginID              NVARCHAR(256),
+	OrganizationNode     HIERARCHYID NULL,
+	OrganizationLevel    AS (OrganizationNode.GetLevel()),
+	JobTitle             NVARCHAR(50),
+	BirthDate            DATE,
+	MaritalStatus        NCHAR(1),
+	Gender               NCHAR(1),
+	HireDate             DATE,
+	SalariedFlag         BIT,
+	VacationHours        SMALLINT,
+	SickLeaveHours       SMALLINT,
+	CurrentFlag          BIT,
+	rowguid              UNIQUEIDENTIFIER,
+	ModifiedDate         DATETIME,
+
+	RowVersion           BIGINT,
+
+	LoadDate             DATETIME DEFAULT(GETDATE())
+);
